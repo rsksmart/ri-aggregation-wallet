1ae7ead4a2cfe96eba076719c3d611ba117463e3
Anni Piragauta <annipi@users.noreply.github.com>
2023-08-30 13:56:14 -0500

    fix: deposit to L1 shown as positive value on history (#99)

--- src/components/TransactionHistoryItem.vue
+++ src/components/TransactionHistoryItem.vue
@@ -366,7 +366,7 @@ export default Vue.extend({
     itReducesMyBalance(): string {
       if (
         (this.transaction.op.type === "Transfer" && this.isSameAddress(this.transaction.op.from)) ||
-        (this.transaction.op.type === "Deposit" && this.isSameAddress(this.transaction.op.to)) ||
+        (this.transaction.op.type === "Deposit" && !this.isSameAddress(this.transaction.op.to)) ||
         this.transaction.op.type === "FullExit"
       ) {
         return "-";
